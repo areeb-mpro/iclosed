624b6e482325b873e69ca24688d24f83e8744822
Areeb Ashfaq <areebashfaq@gmail.com>
2026-09-12 10:56:55 +0000

    Fix import paths in FormBuilderPage - use ../../hooks instead of ../hooks

--- frontend/src/pages/forms/FormBuilderPage.tsx
+++ frontend/src/pages/forms/FormBuilderPage.tsx
@@ -29,8 +29,8 @@ import {
   MoreVertical,
   Copy
 } from 'lucide-react';
-import { useForm as useGetForm, useCreateForm, useUpdateForm } from '../hooks/useForms';
-import { useAuth } from '../hooks/useAuth';
+import { useForm as useGetForm, useCreateForm, useUpdateForm } from '../../hooks/useForms';
+import { useAuth } from '../../hooks/useAuth';
 import { BookingForm, FormField } from '../types';
 import { FORM_FIELD_TYPES, COMMON_FORM_FIELDS } from '../utils/constants';
 import { cn } from '../utils/helpers';
